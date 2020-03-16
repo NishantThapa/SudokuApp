633fb6a0ae287ea2548bdfc820cd168532ff424f
nishantThapa <nishantthapa@icloud.com>
2020-03-16 19:55:19 +0530

    HooksImplemented

--- src/App.js
+++ src/App.js
@@ -1,248 +1,298 @@
-import React, { Component } from 'react';
-import sudokus from './Sudokus'
-import SudokuGenerator from './SudokuGenerator'
-import Board from './Board'
+import React, { useEffect, useState } from "react";
+import sudokus from "./Sudokus";
+import { SudokuGenerator } from "./SudokuGenerator";
+import Board from "./Board";
 
-class Game extends Component {
-	componentDidMount() {
-	this.setState({peep:false})
-	}
-	generate = (level) => {
-		let puzzles
-		switch (level) {
-			case 'Easy':
-				puzzles = sudokus.Easy
-				break
-			case 'Medium':
-				puzzles = sudokus.Medium
-				break
-			case 'Hard':
-				puzzles = sudokus.Hard
-				break
-			default:
-				puzzles = sudokus.Easy
-		}
-		let grid = puzzles[Math.floor(Math.random() * puzzles.length)]
-			, sudoku = new SudokuGenerator(grid).generate()
-			, puzzle = sudoku[0]
-		this.solution = sudoku[1]
-		const origin = new Set()
-		for (let i = 0; i < 9; i++) {
-			for (let j = 0; j < 9; j++) {
-				if (puzzle[i][j]) {
-					origin.add(i + '' + j)
-				}
-			}
-		}
-		this.setState({
-			values: puzzle,
-			level: level,
-			origin: origin,
-			chosen: null,
-			possible: null,
-			filter: new Set(),
-			highlight: new Set(),
-			conflict: new Set(),
-			peep: false,
-		})
-	}
-	componentWillMount() {
-		this.generate('easy')
-	}
-		
-	checkPossible = (i, j) => {
-		let values = this.state.values
-		let allPossible = new Set([...'123456789'])
-		for (let k = 0; k <= 8; k++) {
-			if (k === j) { continue }
-			if (allPossible.has(values[i][k])) {
-				allPossible.delete(values[i][k])
-			}
-		}
-		for (let k = 0; k <= 8; k++) {
-			if (k === i) { continue }
-			if (allPossible.has(values[k][j])) {
-				allPossible.delete(values[k][j])
-			}
-		}
-		let bi = Math.floor(i / 3) * 3,
-			bj = Math.floor(j / 3) * 3
-		for (let m = bi; m < bi + 3; m++) {
-			for (let n = bj; n < bj + 3; n++) {
-				if (m === i && n === j) {
-					continue
-				}
-				if (allPossible.has(values[m][n])) {
-					allPossible.delete(values[m][n])
-				}
-			}
-		}
-		return allPossible
-	}
-	filter = (value) => {
-		let values = this.state.values
-		let filter = new Set()
-		for (let m = 0; m < 9; m++) {
-			for (let n = 0; n < 9; n++) {
-				if (values[m][n] === value) {
-					filter.add(m + '' + n)
-				}
-			}
-		}
-		this.setState({
-			filter: filter,
-			highlight: new Set(),
-			chosen: null
-		})
-	}
-	highlight = (i, j) => {
-		let values = this.state.values
-		let highlight = new Set()
-		for (let k = 0; k < 9; k++) {
-			if (values[i][k]) {
-				highlight.add(i + '' + k)
-			}
-		}
-		for (let k = 0; k < 9; k++) {
-			if (values[k][j]) {
-				highlight.add(k + '' + j)
-			}
-		}
-		let line = Math.floor(i / 3) * 3,
-			row = Math.floor(j / 3) * 3
-		for (let ln = line; ln < line + 3; ln++) {
-			for (let r = row; r < row + 3; r++) {
-				if (values[ln][r]) {
-					highlight.add(ln + '' + r)
-				}
-			}
-		}
-		this.setState({
-			highlight: highlight,
-			filter: new Set()
-		})
-	}
-	solve = () => {
-		debugger
-		if (this.state.peep) {
-			return
-		}
-		// eslint-disable-next-line no-restricted-globals
-		let r = confirm("Are you sure you want to check the answer ? ")
-		if (!r) {
-			return
-		} else {
-			this.setState({
-				values: this.solution,
-				peep: true,
-				conflict: new Set(),
-				highlight: new Set(),
-				filter: new Set(),
-			})
-		}
+const Game = () => {
+  const [solutionValue, setSolutionValue] = useState();
+  const [docState, setDocState] = useState({
+    level: "",
+    origin: new Set(),
+    chosen: null,
+    possible: null,
+    filter: new Set(),
+    highlight: new Set(),
+    conflict: new Set(),
+    peep: false
+  });
 
-	}
-	handleClick = (i, j) => {
-		let values = this.state.values.slice()
-		let thisvalue = values[i].slice()
-		if (this.state.origin.has(i + '' + j)) {
-			this.filter(thisvalue[j])
-			return
-		} else {
-			this.highlight(i, j)
-			let chosen = i + '' + j
-			let possible = Array.from(this.checkPossible(i, j)).toString()
-			this.setState({
-				chosen: chosen,
-				possible: possible,
-				filter: new Set(),
-				check: false
-			});
-		}
-	}
-	handleNumsClick = (i) => {
-		if (this.state.peep) { return }
-		let chosen = this.state.chosen
-		if (!chosen) {
-			this.filter('' + i)
-		} else {
-			let values = this.state.values.slice()
-			if (this.state.origin.has([chosen[0]][chosen[1]])) {
-				this.setState({
-					chosen: null,
-					highlight: new Set()
-				})
-				return
-			}
-			if (i === 'X') {
-				values[chosen[0]][chosen[1]] = null
-			} else {
-				values[chosen[0]][chosen[1]] = '' + i
-			}
-			let conflict = new Set()
-			for (let i = 0; i < 9; i++) {
-				for (let j = 0; j < 9; j++) {
-					if (!values[i][j]) {
-						continue
-					} else {
-						let thisvalue = values[i][j],
-							possible = this.checkPossible(i, j)
-						if (!possible.has(thisvalue)) {
-							conflict.add(i + '' + j)
-						}
-					}
-				}
-			}
-			this.setState(
-				{
-					values: values,
-					highlight: new Set(),
-					conflict: conflict,
-					chosen: null
-				}
-			)
-			if (!this.state.peep && values.toString() === this.solution.toString()) {
-				alert('Congratulations, you have completed this puzzle!')
-				this.setState({
-					peep: true
-				})
-			}
-		}
-	}
-	render() {
-		let choices = [...'123456789'].map((i) => {
-			return <button key={i} className="choice" value={i} onClick={() => this.handleNumsClick(i)}>{i}</button>
-		})
-		let controls = ['Easy', 'Medium', 'Hard'].map((level, index) => {
-			let active = level === this.state.level ? ' active' : ''
-			return <li key={index} className={"level" + active} onClick={() => this.generate(level)}>{level}</li>
-		})
-		return (
-			<div className="game">
-				<ul className="controls">
-					{controls}
-					<li>
-						<button className="clear" onClick={() => this.handleNumsClick("X")}>Select&Delete</button>
-						<button className={"solved"}  onClick={this.solve}>SolveItNow</button>
-					</li>
-				</ul>
-				<div className="main">
+  useEffect(() => {
+    generate("easy");
+  }, []);
 
-					<Board values={this.state.values}
-						origin={this.state.origin}
-						filter={this.state.filter}
-						conflict={this.state.conflict}
-						chosen={this.state.chosen}
-						highlight={this.state.highlight}
-						onClick={this.handleClick} />
-					<div className="right">
-					</div>
-				</div>
-				<ul className="choices">
-					{choices}
-				</ul>
-			</div>
-		);
-	}
-}
-export default Game
+  const generate = async level => {
+    var puzzles;
+    switch (level) {
+      case "Easy":
+        puzzles = sudokus.Easy;
+        break;
+      case "Medium":
+        puzzles = sudokus.Medium;
+        break;
+      case "Hard":
+        puzzles = sudokus.Hard;
+        break;
+      default:
+        puzzles = sudokus.Easy;
+    }
+    let grid = puzzles[Math.floor(Math.random() * puzzles.length)],
+      sudoku = SudokuGenerator(grid),
+      puzzle = sudoku[0];
+
+    setSolutionValue(sudoku[1]);
+
+    const origin = new Set();
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (puzzle[i][j]) {
+          origin.add(i + "" + j);
+        }
+      }
+    }
+
+    await setDocState({
+      values: puzzle,
+      level: level,
+      origin: origin,
+      chosen: null,
+      possible: null,
+      filter: new Set(),
+      highlight: new Set(),
+      conflict: new Set(),
+      peep: false
+    });
+
+  };
+
+  const checkPossible = (i, j) => {
+    let values = docState.values;
+    let allPossible = new Set([..."123456789"]);
+    for (let k = 0; k <= 8; k++) {
+      if (k === j) {
+        continue;
+      }
+      if (allPossible.has(values[i][k])) {
+        allPossible.delete(values[i][k]);
+      }
+    }
+    for (let k = 0; k <= 8; k++) {
+      if (k === i) {
+        continue;
+      }
+      if (allPossible.has(values[k][j])) {
+        allPossible.delete(values[k][j]);
+      }
+    }
+    let bi = Math.floor(i / 3) * 3,
+      bj = Math.floor(j / 3) * 3;
+    for (let m = bi; m < bi + 3; m++) {
+      for (let n = bj; n < bj + 3; n++) {
+        if (m === i && n === j) {
+          continue;
+        }
+        if (allPossible.has(values[m][n])) {
+          allPossible.delete(values[m][n]);
+        }
+      }
+    }
+    return allPossible;
+  };
+
+  const filter = value => {
+    let values = docState.values;
+    let filter = new Set();
+    for (let m = 0; m < 9; m++) {
+      for (let n = 0; n < 9; n++) {
+        if (values[m][n] === value) {
+          filter.add(m + "" + n);
+        }
+      }
+    }
+    setDocState({
+      ...docState,
+      filter: filter,
+      highlight: new Set(),
+      chosen: null
+    });
+  };
+
+  const highlight = (i, j) => {
+    let values = docState.values;
+    let highlight = new Set();
+    for (let k = 0; k < 9; k++) {
+      if (values[i][k]) {
+        highlight.add(i + "" + k);
+      }
+    }
+    for (let k = 0; k < 9; k++) {
+      if (values[k][j]) {
+        highlight.add(k + "" + j);
+      }
+    }
+    let line = Math.floor(i / 3) * 3,
+      row = Math.floor(j / 3) * 3;
+    for (let ln = line; ln < line + 3; ln++) {
+      for (let r = row; r < row + 3; r++) {
+        if (values[ln][r]) {
+          highlight.add(ln + "" + r);
+        }
+      }
+    }
+    setDocState({
+      ...docState,
+      highlight: highlight,
+      filter: new Set()
+    });
+  };
+
+  const solve = () => {
+    if (docState.peep) {
+      return;
+    }
+    // eslint-disable-next-line no-restricted-globals
+    let r = confirm("Are you sure you want to check the answer ? ");
+    if (!r) {
+      return;
+    } else {
+      setDocState({
+        ...docState,
+        values: solutionValue,
+        peep: true,
+        conflict: new Set(),
+        highlight: new Set(),
+        filter: new Set()
+      });
+    }
+  };
+
+  const handleClick = (i, j) => {
+    let values = docState.values.slice();
+    let thisvalue = values[i].slice();
+    if (docState.origin.has(i + "" + j)) {
+      filter(thisvalue[j]);
+      return;
+    } else {
+      highlight(i, j);
+      let chosen = i + "" + j;
+      let possible = Array.from(checkPossible(i, j)).toString();
+      setDocState({
+        ...docState,
+        chosen: chosen,
+        possible: possible,
+        filter: new Set(),
+        check: false
+      });
+    }
+  };
+
+  const handleNumsClick = i => {
+    if (docState.peep) {
+      return;
+    }
+    let chosen = docState.chosen;
+    if (!chosen) {
+      filter("" + i);
+    } else {
+      let values = docState.values.slice();
+      if (docState.origin.has([chosen[0]][chosen[1]])) {
+        setDocState({
+          ...docState,
+          chosen: null,
+          highlight: new Set()
+        });
+        return;
+      }
+      if (i === "X") {
+        values[chosen[0]][chosen[1]] = null;
+      } else {
+        values[chosen[0]][chosen[1]] = "" + i;
+      }
+      let conflict = new Set();
+      for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          if (!values[i][j]) {
+            continue;
+          } else {
+            let thisvalue = values[i][j],
+              possible = checkPossible(i, j);
+            if (!possible.has(thisvalue)) {
+              conflict.add(i + "" + j);
+            }
+          }
+        }
+      }
+      setDocState({
+        ...docState,
+        values: values,
+        highlight: new Set(),
+        conflict: conflict,
+        // chosen: null
+      });
+      if (!docState.peep && values.toString() === solutionValue.toString()) {
+        alert("Congratulations, you have completed this puzzle!");
+        setDocState({
+          ...docState,
+          peep: true
+        });
+      }
+    }
+  };
+
+  const choices = [..."123456789"].map(i => {
+    return (
+      <button
+        key={i}
+        className="choice"
+        value={i}
+        onClick={() => handleNumsClick(i)}
+      >
+        {i}
+      </button>
+    );
+  });
+
+  const controls = ["Easy", "Medium", "Hard"].map((level, index) => {
+    let active = level === docState.level ? " active" : "";
+    return (
+      <li
+        key={index}
+        className={"level" + active}
+        onClick={() => generate(level)}
+      >
+        {level}
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <ul className="controls">
+        {controls}
+        <li>
+          <button className="clear" onClick={() => handleNumsClick("X")}>
+            Select & Delete
+          </button>
+          <button className={"solved"} onClick={solve}>
+            Solve It Now
+          </button>
+        </li>
+      </ul>
+      <div className="main">
+        <Board
+          values={docState.values}
+          origin={docState.origin}
+          filter={docState.filter}
+          conflict={docState.conflict}
+          chosen={docState.chosen}
+          highlight={docState.highlight}
+          onClick={handleClick}
+          onChangeNums={handleNumsClick}
+        />
+        <div className="right"></div>
+      </div>
+      <ul className="choices">{choices}</ul>
+    </div>
+  );
+};
+
+export default Game;
--- src/Board.js
+++ src/Board.js
@@ -1,35 +1,65 @@
-import React, { Component } from 'react';
-class Board extends Component {
-	getsquares = (rowindex) => {
-		let squares = [...'012345678'].map((i, squareindex) => {
-			let cord = rowindex + '' + squareindex
-				, className = 'square'
-			if (this.props.origin.has(cord)) { className += ' origin' }
-			if (this.props.highlight.has(cord)) { className += ' highlight' }
-			if (this.props.filter.has(cord)) { className += ' filter' }
-			if (this.props.conflict.has(cord)) { className += ' conflict' }
-			if (this.props.chosen === cord) { className += ' chosen' }
-			return (
-				<button key={squareindex} className={className} onClick={() => this.props.onClick(rowindex, squareindex)}>
-					{this.props.values[rowindex][squareindex]}
-				</button>
-			)
-		})
-		return (
-			<div key={rowindex} className={'row ' + rowindex}>
-				{squares}
-			</div>
-		)
-	}
-	render() {
-		let rows = [...'012345678'].map((i, rowindex) => {
-			return this.getsquares(rowindex)
-		})
-		return (
-			<div className='board'>
-				{rows}
-			</div>
-		)
-	}
-}
-export default Board
+import React from "react";
+
+const Board = props => {
+  const getsquares = rowindex => {
+    const squares = [..."012345678"].map((i, squareindex) => {
+      let cord = rowindex + "" + squareindex,
+        className = "square";
+      if (props.origin.has(cord)) {
+        className += " origin";
+      }
+      if (props.highlight.has(cord)) {
+        className += " highlight";
+      }
+      if (props.filter.has(cord)) {
+        className += " filter";
+      }
+      if (props.conflict.has(cord)) {
+        className += " conflict";
+      }
+      if (props.chosen === cord) {
+        className += " chosen";
+      }
+
+      return (
+        <button
+          key={squareindex}
+          className={className}
+          onClick={() => props.onClick(rowindex, squareindex)}
+        >
+          {input(props.values && props.values[rowindex][squareindex])}
+        </button>
+      );
+    });
+
+    return (
+      <div key={rowindex} className={"row " + rowindex}>
+        {squares}
+      </div>
+    );
+  };
+
+  const handleChange = e => {
+    const range = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const val = parseInt(e.target.value);
+    if (range.indexOf(val) === -1) return;
+    props.onChangeNums(val);
+  };
+
+  const input = val => (
+    <input
+      className="square-input"
+      disabled={val}
+      value={val ? val : ""}
+      onChange={e => handleChange(e)}
+    />
+  );
+
+  const rows = [..."012345678"].map((i, rowindex) => {
+    return getsquares(rowindex);
+  });
+
+  return <div className="board">{rows}</div>;
+};
+
+export default Board;
