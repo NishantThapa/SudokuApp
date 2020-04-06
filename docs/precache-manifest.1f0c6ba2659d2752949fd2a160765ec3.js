6f04f9dd19a5167250c93e1225875d4fe00b9f24
NishantThapa <nishantthapa@icloud.com>
2020-04-06 23:35:30 +0530

    buildDeployed

---
+++ docs/precache-manifest.1f0c6ba2659d2752949fd2a160765ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b42fb42ab165f22f268afd12907029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "671aff0f2bab653a9434",
+    "url": "/static/css/main.b4e2e1dc.chunk.css"
+  },
+  {
+    "revision": "c662974401844a6db2b4",
+    "url": "/static/js/2.7af884b9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7af884b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671aff0f2bab653a9434",
+    "url": "/static/js/main.d09151a1.chunk.js"
+  },
+  {
+    "revision": "74ecf57cab4b19bc151a",
+    "url": "/static/js/runtime-main.dce311a8.js"
+  }
+]);
